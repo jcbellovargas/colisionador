5d0b5aa1165a6d3f8087322257750fa93cab490d
amarseroEscritorio <amarsero@hotmail.com>
2016-11-12 19:02:08 -0300

    Acá dejo la grafiquera, toca todo lo que quieras

---
+++ Grafiquera/Grafiquera/Form1.Designer.cs
@@ -0,0 +1,172 @@
+﻿namespace Grafiquera
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.Windows.Forms.DataVisualization.Charting.ChartArea chartArea1 = new System.Windows.Forms.DataVisualization.Charting.ChartArea();
+            System.Windows.Forms.DataVisualization.Charting.Legend legend1 = new System.Windows.Forms.DataVisualization.Charting.Legend();
+            System.Windows.Forms.DataVisualization.Charting.Series series1 = new System.Windows.Forms.DataVisualization.Charting.Series();
+            System.Windows.Forms.DataVisualization.Charting.Series series2 = new System.Windows.Forms.DataVisualization.Charting.Series();
+            this.chart1 = new System.Windows.Forms.DataVisualization.Charting.Chart();
+            this.BtnBuscar = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.BtnEmpezar = new System.Windows.Forms.Button();
+            this.openFileDialog1 = new System.Windows.Forms.OpenFileDialog();
+            this.fileSystemWatcher1 = new System.IO.FileSystemWatcher();
+            ((System.ComponentModel.ISupportInitialize)(this.chart1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.fileSystemWatcher1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // chart1
+            // 
+            chartArea1.AxisX.IsMarginVisible = false;
+            chartArea1.AxisX.Title = "Segundo";
+            chartArea1.AxisX2.Title = "Segundos";
+            chartArea1.AxisY.Interval = 15D;
+            chartArea1.AxisY.IsMarginVisible = false;
+            chartArea1.AxisY.LineDashStyle = System.Windows.Forms.DataVisualization.Charting.ChartDashStyle.NotSet;
+            chartArea1.AxisY.MajorGrid.LineWidth = 2;
+            chartArea1.AxisY.MajorTickMark.LineColor = System.Drawing.Color.Blue;
+            chartArea1.AxisY.MajorTickMark.LineWidth = 2;
+            chartArea1.AxisY.MajorTickMark.TickMarkStyle = System.Windows.Forms.DataVisualization.Charting.TickMarkStyle.AcrossAxis;
+            chartArea1.AxisY.Maximum = 150D;
+            chartArea1.AxisY.Minimum = 0D;
+            chartArea1.AxisY.MinorGrid.Enabled = true;
+            chartArea1.AxisY.MinorGrid.LineColor = System.Drawing.Color.Silver;
+            chartArea1.AxisY.MinorGrid.LineDashStyle = System.Windows.Forms.DataVisualization.Charting.ChartDashStyle.Dash;
+            chartArea1.AxisY.Title = "Cuadros por segundo";
+            chartArea1.AxisY2.IsMarginVisible = false;
+            chartArea1.AxisY2.MajorGrid.LineWidth = 2;
+            chartArea1.AxisY2.MajorTickMark.LineColor = System.Drawing.Color.Maroon;
+            chartArea1.AxisY2.MajorTickMark.LineDashStyle = System.Windows.Forms.DataVisualization.Charting.ChartDashStyle.Dash;
+            chartArea1.AxisY2.Maximum = 400D;
+            chartArea1.AxisY2.Minimum = 0D;
+            chartArea1.AxisY2.Title = "Bolas";
+            chartArea1.Name = "ChartArea1";
+            this.chart1.ChartAreas.Add(chartArea1);
+            legend1.BackColor = System.Drawing.Color.Transparent;
+            legend1.IsTextAutoFit = false;
+            legend1.Name = "Legend1";
+            legend1.Position.Auto = false;
+            legend1.Position.Height = 8.26087F;
+            legend1.Position.Width = 16.27907F;
+            legend1.Position.X = 6F;
+            legend1.Position.Y = 80F;
+            this.chart1.Legends.Add(legend1);
+            this.chart1.Location = new System.Drawing.Point(-37, 12);
+            this.chart1.Name = "chart1";
+            series1.BorderWidth = 5;
+            series1.ChartArea = "ChartArea1";
+            series1.ChartType = System.Windows.Forms.DataVisualization.Charting.SeriesChartType.Spline;
+            series1.Legend = "Legend1";
+            series1.Name = "FPS";
+            series2.BorderWidth = 6;
+            series2.ChartArea = "ChartArea1";
+            series2.ChartType = System.Windows.Forms.DataVisualization.Charting.SeriesChartType.Spline;
+            series2.LabelToolTip = "#VAL";
+            series2.Legend = "Legend1";
+            series2.MarkerSize = 30;
+            series2.Name = "Cantidad Bolas";
+            series2.YAxisType = System.Windows.Forms.DataVisualization.Charting.AxisType.Secondary;
+            this.chart1.Series.Add(series1);
+            this.chart1.Series.Add(series2);
+            this.chart1.Size = new System.Drawing.Size(957, 667);
+            this.chart1.TabIndex = 0;
+            this.chart1.Text = "chart1";
+            // 
+            // BtnBuscar
+            // 
+            this.BtnBuscar.Location = new System.Drawing.Point(32, 13);
+            this.BtnBuscar.Name = "BtnBuscar";
+            this.BtnBuscar.Size = new System.Drawing.Size(75, 23);
+            this.BtnBuscar.TabIndex = 1;
+            this.BtnBuscar.Text = "Buscar...";
+            this.BtnBuscar.UseVisualStyleBackColor = true;
+            this.BtnBuscar.Click += new System.EventHandler(this.BtnBuscar_Click);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(113, 15);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.ReadOnly = true;
+            this.textBox1.Size = new System.Drawing.Size(578, 20);
+            this.textBox1.TabIndex = 2;
+            // 
+            // BtnEmpezar
+            // 
+            this.BtnEmpezar.Location = new System.Drawing.Point(697, 13);
+            this.BtnEmpezar.Name = "BtnEmpezar";
+            this.BtnEmpezar.Size = new System.Drawing.Size(75, 23);
+            this.BtnEmpezar.TabIndex = 3;
+            this.BtnEmpezar.Text = "Empezar";
+            this.BtnEmpezar.UseVisualStyleBackColor = true;
+            this.BtnEmpezar.Click += new System.EventHandler(this.BtnEmpezar_Click);
+            // 
+            // openFileDialog1
+            // 
+            this.openFileDialog1.FileName = "openFileDialog1";
+            this.openFileDialog1.Filter = "\"Text files (*.txt)|*.txt|All files (*.*)|*.*\"";
+            this.openFileDialog1.FileOk += new System.ComponentModel.CancelEventHandler(this.openFileDialog1_FileOk);
+            // 
+            // fileSystemWatcher1
+            // 
+            this.fileSystemWatcher1.EnableRaisingEvents = true;
+            this.fileSystemWatcher1.SynchronizingObject = this;
+            this.fileSystemWatcher1.Changed += new System.IO.FileSystemEventHandler(this.fileSystemWatcher1_Changed);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.White;
+            this.ClientSize = new System.Drawing.Size(884, 652);
+            this.Controls.Add(this.BtnEmpezar);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.BtnBuscar);
+            this.Controls.Add(this.chart1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.chart1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.fileSystemWatcher1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.DataVisualization.Charting.Chart chart1;
+        private System.Windows.Forms.Button BtnBuscar;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Button BtnEmpezar;
+        private System.Windows.Forms.OpenFileDialog openFileDialog1;
+        private System.IO.FileSystemWatcher fileSystemWatcher1;
+    }
+}
+
---
+++ Grafiquera/Grafiquera/Form1.cs
@@ -0,0 +1,147 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+
+namespace Grafiquera
+{
+    public partial class Form1 : Form
+    {
+        bool andando;
+        System.IO.FileSystemWatcher file;
+        Archivo archivo;
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void BtnBuscar_Click(object sender, EventArgs e)
+        {
+
+            openFileDialog1.ShowDialog();
+        }
+
+        private void openFileDialog1_FileOk(object sender, CancelEventArgs e)
+        {
+            textBox1.Text = openFileDialog1.FileName;
+        }
+
+
+        private void BtnEmpezar_Click(object sender, EventArgs e)
+        {
+            if (andando)
+            {
+                andando = false;
+                file.Dispose();
+                archivo.Dispose();
+                BtnEmpezar.Text = "Empezaar";
+
+            }
+            else
+            {
+                chart1.Series[0].Points.Clear();
+                chart1.Series[1].Points.Clear();
+                if (!System.IO.File.Exists(textBox1.Text)) throw new Exception("El archivo no existe");
+                andando = true;
+                BtnEmpezar.Text = "Detener";
+                Empezar();
+            }
+        }
+
+        private void Empezar()
+        {
+
+            archivo = new Archivo(textBox1.Text);
+            file = new System.IO.FileSystemWatcher(Path.GetDirectoryName(textBox1.Text),Path.GetFileName(textBox1.Text));
+            file.EnableRaisingEvents = true;
+            file.SynchronizingObject = this;
+            file.Changed += new System.IO.FileSystemEventHandler(this.ArchivoActualizado);
+            InterpretarTexto(archivo.ObtenerHastaFinal());
+        }
+
+
+
+        private void InterpretarTexto(string texto)
+        { 
+            string[] partes = texto.Split(new Char[] {';'});
+
+            for (int i = 0; i < partes.Length; i++)
+            {
+                if (partes[i] == "") continue;
+                string[] datos = partes[i].Split(new Char[] {','});
+                AgregarAlGrafico(datos[0],datos[1]);
+            }
+        }
+
+        private void AgregarAlGrafico(string dato1,string dato2)
+        {
+            chart1.Series[0].Points.AddY(dato1);//FPS
+            chart1.Series[1].Points.AddY(dato2);//CantBolas
+        }
+
+
+        private void ArchivoActualizado(object sender, System.IO.FileSystemEventArgs e)
+        {
+            InterpretarTexto(archivo.ObtenerHastaFinal());
+        }
+
+        private void fileSystemWatcher1_Changed(object sender, System.IO.FileSystemEventArgs e)
+        {
+
+        }
+
+
+
+    }
+
+    public class Archivo : IDisposable
+    {
+
+        private System.IO.FileStream _fileStream;
+        private System.IO.StreamReader _reader;
+
+
+        //Start position is from where to start reading first time. consequent read are managed by the Stream reader
+        public Archivo(string direccion, long posicion = 0)
+        {
+            //Open the file as FileStream
+            _fileStream = new FileStream(direccion, FileMode.Open, FileAccess.Read, FileShare.ReadWrite);
+            _reader = new StreamReader(_fileStream);
+            //Set the starting position
+            _fileStream.Position = posicion;
+        }
+
+
+        //Get the current offset. You can save this when the application exits and on next reload
+        //set startPosition to value returned by this method to start reading from that location
+        public long CurrentOffset
+        {
+            get { return _fileStream.Position; }
+        }
+
+        //Returns the lines added after this function was last called
+        public string ObtenerHastaFinal()
+        {
+            return _reader.ReadToEnd();
+        }
+
+
+
+        public void Dispose()
+        {
+            _reader.Close();
+            _fileStream.Close();
+        }
+    }
+}
